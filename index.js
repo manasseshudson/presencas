c5df3fa390418e95c602ac8a98b9f3fdf0061dfe
Manasses <manasseshudson@gmail.com>
2025-12-18 10:49:49 -0300

    dd

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ require('dotenv').config();
 app.use(cors());
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
